03020307dc3dc3387b22ae9c0c98c4cc983df447
SofaShar <sharova_sonya@inbox.ru>
2026-05-26 14:03:01 +0300

    new user

--- project/db.php
+++ project/db.php
@@ -1,6 +1,6 @@
 <?php
-$user = 'u68857'; 
-$password = '9940611'; 
+$user = 'user1'; 
+$password = '123'; 
 try {
     $pdo = new PDO('mysql:host=localhost;dbname=u68857', $user, $password,
         [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
