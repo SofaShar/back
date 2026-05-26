7235b26d87aa56751f1472e3f3a94e26f928d7c0
SofaShar <sharova_sonya@inbox.ru>
2026-05-26 14:12:50 +0300

    Update db.php

--- project/db.php
+++ project/db.php
@@ -2,7 +2,7 @@
 $user = 'user1'; 
 $password = '123'; 
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=u68857', $user, $password,
+    $pdo = new PDO('mysql:host=localhost;dbname=form_db', $user, $password,
         [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 } catch (PDOException $e) {
     die("<p style='color:red;'>Ошибка подключения к базе данных: " . $e->getMessage() . "</p>");
